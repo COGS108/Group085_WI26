65fa8bbab8433eada4faa14ae44f5b0d5d5e39f8
Tom Klappenecker <165523048+tom-klappenecker@users.noreply.github.com>
2026-02-26 01:26:33 +0000

    Added get_gdrive function

--- modules/get_data.py
+++ modules/get_data.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from tqdm import tqdm
+import gdown
 
 def get_raw(file_list, destination_directory):
     """
@@ -36,6 +37,36 @@ def get_raw(file_list, destination_directory):
                         if chunk:  # filter out keep-alive new chunks
                             f.write(chunk)
                             pbar.update(len(chunk))
+            print(f"Successfully downloaded: {filename}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error downloading {filename}: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred for {filename}: {e}")
+
+
+def get_gdrive(file_list, destination_directory):
+    """
+    Downloads a list of files from Google Drive shareable links to a specified destination directory with progress bars.
+
+    Args:
+        file_list (list): A list of dictionaries, where each dictionary
+                          contains 'url' (the URL of the file) and 'filename'
+                          (the desired local filename).
+        destination_directory (str): The path to the directory where files
+                                     should be saved.
+    """
+    if not os.path.exists(destination_directory):
+        print(f"Error directory {destination_directory} does not exist")
+        return
+
+    for file_info in tqdm(file_list, desc="Overall Download Progress"):
+        url = file_info['url']
+        filename = file_info['filename']
+        local_filepath = os.path.join(destination_directory, filename)
+
+        try:
+            # learned how to use gdown from the official repository: https://github.com/wkentaro/gdown
+            gdown.download(url, local_filepath)
             print(f"Successfully downloaded: {filename}")
         except requests.exceptions.RequestException as e:
             print(f"Error downloading {filename}: {e}")
